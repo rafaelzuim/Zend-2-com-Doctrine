78fd8e12ed7d649c1c8c07cdbadced16065355e0
rafael_zuim <rafael.czuim@gmail.com>
2015-02-19 17:13:15 -0200

    Aula 01 - Revisando 	04. Ativando Modules 	05. Criando a SONUser

--- config/application.config.php
+++ config/application.config.php
@@ -3,6 +3,11 @@ return array(
     // This should be an array of module namespaces used in the application.
     'modules' => array(
         'Application',
+        'DoctrineModule',
+        'DoctrineORMModule',
+         'DoctrineDataFixtureModule',
+        'SONBase',
+        #'SONUser',
 
     ),
 
---
+++ module/SONBase/Config/module.config.php
@@ -0,0 +1,3 @@
+<?php
+namespace SONBase;
+return array();
